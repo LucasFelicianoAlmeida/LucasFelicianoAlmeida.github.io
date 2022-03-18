524e32c3898ead821d056990616e4f275ae26f65
LucasFelicianoAlmeida <lucasfeliciano326@gmail.com>
2022-03-18 19:37:19 -0300

    first commit, header page

---
+++ portfolio/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {Route, RouterModule} from '@angular/router'
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+
+const routes: Route[] = 
+[
+  {path: 'home', component: HomeComponent }
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    NavMenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule { }
