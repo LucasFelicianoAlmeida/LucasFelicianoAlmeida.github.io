3967938ae52e684399fb292d71bc9704a215ceba
LucasFelicianoAlmeida <lucasfeliciano326@gmail.com>
2022-04-27 21:42:05 -0300

    archive section resized

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-// import {BehaviorSubject} from '@angular/rxjs'
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-  // BehaviorSubject
+export class HomeComponent implements OnInit, OnDestroy {
+  public IndexCounter  = new BehaviorSubject<number>(0) 
   constructor() { }
+  
+
+  private counter = 0
+
+  private interval = setInterval(() => {
+    this.counter++;
+    if(this.counter > 4)
+      this.counter = 0
+    this.IndexCounter.next(this.counter);
+  },3000)
+
 
   ngOnInit(): void {
   }
+
+  ngOnDestroy(): void {
+    clearInterval(this.interval);
+  }
+
+  
 
 }
